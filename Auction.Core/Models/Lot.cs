0045b2000c4505a4bc9310a5bdbf68e740a89cdc
DoNiFF <prodoniff@gmail.com>
2024-09-16 05:34:23 +0300

    Add new method 'TryDoBet'

--- Auction.Core/Models/Lot.cs
+++ Auction.Core/Models/Lot.cs
@@ -1,4 +1,5 @@
-﻿namespace Auction.Core.Models;
+﻿using FluentResults;
+namespace Auction.Core.Models;
 
 /// <summary>
 /// Lot Model
@@ -18,6 +19,11 @@ public class Lot
     public string Name { get; init; }
 
     /// <summary>
+    /// Code
+    /// </summary>
+    public string Code { get; init; }
+
+    /// <summary>
     /// Lot name
     /// </summary>
     public string Description { get; init; }
@@ -30,10 +36,29 @@ public class Lot
     /// <summary>
     /// Lot bets
     /// </summary>
-    public List<Bet> Bets { get; init; }
+    public IReadOnlyCollection<Bet> Bets => _Bets;
+
+    private List<Bet> _Bets = new List<Bet>();
 
     /// <summary>
     /// Lot pictures
     /// </summary>
-    public IReadOnlyCollection<string> Images { get; init; }
+    public IReadOnlyCollection<string> Images { get; init; } = new List<string>();
+
+    /// <summary>
+    /// Attempting to place a bet
+    /// </summary>
+    /// <param name="bet">Bet</param>
+    /// <returns>The result of the operation execution. If bidding on the lot is completed or a bid with this size has already been made, it will return an Fail</returns>
+    public Result TryDoBet(Bet bet)
+    {
+        if (Status != LotStatus.Complete)
+            return Result.Fail("It is impossible to bid on this lot, because the auction is closed");
+
+        if (_Bets.Any(b => b.Amount >= bet.Amount))
+            return Result.Fail("Your bid has been overridden, try again later");
+
+        _Bets.Add(bet); 
+        return Result.Ok();
+    }
 }
